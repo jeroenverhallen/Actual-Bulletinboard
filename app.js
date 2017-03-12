340c7b0ce5906cf17e1543c50299fcfa1c58af41
jeroenverhallen <jeroenjverhallen@gmail.com>
2017-03-12 22:47:11 +0100

    finished last couple details and added some comments to my code

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use( session( {
     }
 } ) )
 
-
+// we define what the tables in the database look like
 const user = db.define( 'user', {
     username: sequelize.STRING,
     email: sequelize.STRING,
@@ -46,6 +46,7 @@ const comment = db.define( 'comment', {
     message: sequelize.STRING
 } )
 
+// We set relations between the different tables
 post.belongsTo( user )
 user.hasMany( post )
 comment.belongsTo( post )
@@ -57,122 +58,141 @@ app.set( 'views', __dirname + '/views' )
 app.set( 'view engine', 'pug' )
 
 
-
+// default path
 app.get( '/', ( req, res ) => {
     console.log('THe session is ', req.session)
     res.render( 'index', {
+// If a user is logged in, he stays logged in when changing subpage
         user: req.session.user
     } )
 } )
 
+// the page for registering a new user
 app.get( '/new-user', ( req, res ) => {
     res.render( 'new-user', {
         user: req.session.user
     } )
 } )
 
-app.get( '/messageboard', ( req, res ) => {
-    post.findAll().then( allposts => { 
-        res.render( 'messageboard', { posts: allposts, user: req.session.user } )
-    } ) 
-} )
-
+// login
 app.get( '/login', ( req, res ) => {
     res.render( 'login', {
         user: req.session.user
     } )
 } )
 
-app.get( '/your-posts', ( req, res ) => {
-    post.findAll( { where: { userId : req.session.user.id } } ).then( allposts => { 
-        res.render( 'userposts', { posts: allposts, user: req.session.user } )
-    } ) 
-} )
-
-app.get( '/newpost', ( req, res ) => {
-    res.render( 'newpost', {
-        user: req.session.user
+// view all posts
+    app.get( '/messageboard', ( req, res ) => {
+        post.findAll().then( allposts => { 
+            res.render( 'messageboard', { posts: allposts, user: req.session.user } )
+        } ) 
     } )
-} )
 
-app.get( '/index', ( req, res ) => {
-    res.render( 'index', {
-        user: req.session.user
+// view all posts made by logged in user
+    app.get( '/your-posts', ( req, res ) => {
+        post.findAll( { where: { userId : req.session.user.id } } ).then( allposts => { 
+            res.render( 'userposts', { posts: allposts, user: req.session.user } )
+        } ) 
     } )
-} )
 
-app.get( '/:title', ( req, res) => {
-    console.log( req.params.title )
-    post.findOne( {
-        where: {
-            title: req.params.title
-        }
-    } ).then( singlepost  => {
-        res.render( '1post', {
-            post: singlepost,
+// create a new post
+    app.get( '/newpost', ( req, res ) => {
+        res.render( 'newpost', {
             user: req.session.user
         } )
     } )
-} )
 
-app.post( ':title', ( req, res ) => {
-    console.log( req.body.message )
-    comment.create( {
-        message: req.body.message,
-        userId: req.session.user.id
-    } ).then ( f => {
-        res.render( '1post', { user: req.session.user } )
-    } )
-} )
-
-app.post( '/new-user', ( req, res ) => {
-    return user.create( {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    } ).then ( f => {
-        res.render( 'login', {
-        user: req.session.user
-    } )
-    } )
-} )
-
-app.post( '/newpost', ( req, res ) => {
-    return post.create( {
-        title: req.body.title,
-        message: req.body.message,
-        userId: req.session.user.id
-    } ).then ( f => {
+// landing page
+    app.get( '/index', ( req, res ) => {
         res.render( 'index', {
-        user: req.session.user
+            user: req.session.user
+        } )
     } )
-    } )
-} )
 
-app.post( '/login', ( req, res ) => {
-    console.log('Posted username is ', req.body.username)
-    user.findOne( {
-        where: {
-            username: req.body.username
-        }
-    } ).then( theuser => {
-        console.log('User from database is ',theuser )
-        if ( theuser.password == req.body.password ) {
-            req.session.user = theuser
-            console.log('Login session set to', req.session)
-            res.render( 'index', {
-               user: theuser 
+// the session is destroyed when logging out, to prevent you from automatically being logged in again when going to a previously visited page
+    app.get( '/logout', ( req, res ) => {
+        console.log( 'now logging out' )
+        req.session.destroy( )
+        res.render( 'index' )
+    } )
+
+// this shows an individual post with comments and authors of both post and comments    
+    app.get( '/singlepost/:title', ( req, res) => {
+            post.findOne( {
+            where: {
+                title: req.params.title
+            }, include: [
+                { model: comment, include: [ user ] },
+                { model: user }
+            ]
+        } )
+        .then( singlepost => {
+            console.log( 'the post with comments is ', singlepost.get({plain: true}) )
+            res.render( 'comment', {
+                post: singlepost,
+                user: req.session.user
+                } )
             } )
-        } else {
-            res.render( 'login' )
-        }
-    } ).catch(console.log.bind(console))
-} )
+    } )
 
-  app.get( '/logout', ( req, res ) => {
-      req.session.destroy( )
-      res.render( 'index' )
-  } )
+    app.post( '/new-user', ( req, res ) => {
+        user.create( {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        } ).then ( f => {
+            res.render( 'login', {
+            user: req.session.user
+        } )
+        } )
+    } )
+
+    app.post( '/newpost', ( req, res ) => {
+        post.create( {
+            title: req.body.title,
+            message: req.body.message,
+            // the post is linked to the logged in user
+            userId: req.session.user.id
+        } ).then ( f => {
+            res.render( 'index', {
+            user: req.session.user
+        } )
+        } )
+    } )
+
+    app.post( '/login', ( req, res ) => {
+        console.log('Posted username is ', req.body.username)
+        // it looks for a user with the name you filled in in the form and then checks whether the password checks out
+        user.findOne( {
+            where: {
+                username: req.body.username
+            }
+        } ).then( theuser => {
+            console.log('User from database is ',theuser )
+            if ( theuser.password == req.body.password ) {
+                req.session.user = theuser
+                console.log('Login session set to', req.session)
+                res.render( 'index', {
+                user: theuser 
+                } )
+            } else {
+                res.render( 'login' )
+            }
+        } ).catch(console.log.bind(console))
+    } )
+
+    app.post( '/singlepost/:title', ( req, res ) => {
+        console.log( 'your comment is ', req.body.comment )
+        console.log( 'the user is ', req.session.user.username )
+        console.log( 'the id of the post is ', req.body.postId )
+        comment.create( {
+            message: req.body.comment,
+            userId: req.session.user.id,
+            postId: req.body.postId
+        } ).then ( ( user, post ) => {
+            res.render( 'index', { user: req.session.user } )
+        } )
+    } )
 
 app.listen( 3000, f => {
     console.log( 'Yo daaaawg! check out this sweeeet blog application on localhost: 3000' )
